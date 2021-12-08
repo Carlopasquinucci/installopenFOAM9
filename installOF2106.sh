7b570bc65d50fd9c16aa123981319dce309035ff
CarloPa <54579322+Carlopasquinucci@users.noreply.github.com>
2021-12-08 11:19:18 +0100

    add installOF2106.sh

---
+++ installOF2106.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# How to install openFOAM 2106
+
+cd $HOME
+sudo wget -q -O - https://dl.openfoam.com/add-debian-repo.sh | sudo bash
+sudo apt-get install openfoam2106-default
+
+
+echo 'alias of2106="source /usr/lib/openfoam/openfoam2106/etc/bashrc"'>> .bashrc
+source .bashrc
+
+
